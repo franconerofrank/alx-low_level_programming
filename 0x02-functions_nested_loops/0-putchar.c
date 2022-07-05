77f4d9857b3679201a487d274f24f84de6db38fe
franconerofrank <francisoguaju90@gmail.com>
2022-07-05 08:02:46 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,10 @@
 #include"main.h"
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: prints _putchar using putchar prototype
- *     *
- *      * Return: Always 0 (Success)
- *      */
+ * main - Entry point
+ * Description: prints _putchar using putchar prototype
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
