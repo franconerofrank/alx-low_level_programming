85a783fed4c3874e2c8fb8a7b0669e6051b20c16
franconerofrank <francisoguaju90@gmail.com>
2022-07-12 15:20:48 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,21 @@
-#include "holberton.h"
+#include "main.h"
+#include <string.h>
 
 /**
- * puts_half - Entry point
- * Desc: Entry
- *@str: pointer
- * Return: Always 0 (Success)
+ * puts_half -> puts half
+ * @str: string param
  */
+
 void puts_half(char *str)
 {
-	int i;
-	int k;
+	int x, y, i;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-	}
-	i--;
-	k = i % 2;
-	i = (i - k) / 2;
-	for (i = i + 1; str[i] != '\0'; i++)
-	{
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
