6ff7d152a8bbcde48687009e4983c6a326fec6b4
franconerofrank <francisoguaju90@gmail.com>
2022-07-12 14:32:42 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,25 @@
 #include "holberton.h"
+
 /**
- * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
- * @str: input
- * Return: half of input
+ * puts_half - Entry point
+ * Desc: Entry
+ *@str: pointer
+ * Return: Always 0 (Success)
  */
 void puts_half(char *str)
 {
+	int i;
+	int k;
 
-	int n, m, len;
-
-	len = 0;
-
-	for (n = 0; str[n] != '\0'; n++)
-		len++;
-
-	m = (len / 2);
-
-	if ((len % 2) == 1)
-		m = ((len + 1) / 2);
-
-	for (n = m; str[n] != '\0'; n++)
-		_putchar(str[n]);
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+	}
+	i--;
+	k = i % 2;
+	i = (i - k) / 2;
+	for (i = i + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
