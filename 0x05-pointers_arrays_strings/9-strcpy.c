1cca72a517ea48244c377220e2e547910a4b8a72
franconerofrank <francisoguaju90@gmail.com>
2022-07-12 14:38:04 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 
 /**
- * _strcpy - Entry point
- * Desc: Entry
- *@dest: pointer
- *@src: pointer
- * Return: Always 0 (Success)
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = -1;
 
-	for (i = 0 ; src[i] != '\0' ; i++)
-	{
-	dest[i] = src[i];
-	}
-	*(dest + i) = '\0';
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
 	return (dest);
 }
