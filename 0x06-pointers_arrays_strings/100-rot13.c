56041a62da8df9af85d35f4965b6572c8773e6bc
franconerofrank <francisoguaju90@gmail.com>
2022-07-15 04:24:06 -0700

    Don't hate the hacker, hate the code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,28 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 /**
- * rot13 - encode using rot13
- * @s: variable
- * Return: Always 0 (Success)
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *rot13(char *s)
 {
-	int i = 0, i2 = 0;
-	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+int count = 0, i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + i) != 0)
-	{
-		for (i2 = 0; i2 <= 52; i2++)
-		{
-			if (*(s + i) == alpha[i2])
-			{
-				*(s + i) = alpha2[i2];
-				break;
-			}
-		}
-				i++;
-	}
-		return (s);
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 52; i++)
+{
+if (*(s + count) == alphabet[i])
+{
+*(s + count) = rot13[i];
+break;
+}
+}
+count++;
+}
+
+return (s);
 }
